5b4297bd767068cca372667af935588963543ab5
goran <>
2022-06-20 13:23:11 +0200

    .env added

--- public/index.php
+++ public/index.php
@@ -2,8 +2,12 @@
 
 require_once __DIR__ .'/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
 use app\classes\Application;
 use app\controllers\StudentController;
+
 $app = new Application();
 
 $app->router->get('/', [StudentController::class, 'getStudent']);
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit2be28662809c8721349b7297f1834876
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit2be28662809c8721349b7297f1834876', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit2be28662809c8721349b7297f1834876', 'loadClassLoader'));
@@ -50,6 +52,24 @@ class ComposerAutoloaderInit2be28662809c8721349b7297f1834876
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit2be28662809c8721349b7297f1834876::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire2be28662809c8721349b7297f1834876($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire2be28662809c8721349b7297f1834876($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
